2671244e70585fec06f4a37f9f0076f6b602df3a
abdoulaye150199 <abdoulaye150199@gmail.com>
2025-11-08 13:40:02 +0000

    Initial commit - API Gestion Compte OM

---
+++ src/main/java/com/example/gesioncompteom/GesionCompteOmApplication.java
@@ -0,0 +1,13 @@
+package com.example.gesioncompteom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GesionCompteOmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GesionCompteOmApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/gesioncompteom/GesionCompteOmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gesioncompteom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GesionCompteOmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
